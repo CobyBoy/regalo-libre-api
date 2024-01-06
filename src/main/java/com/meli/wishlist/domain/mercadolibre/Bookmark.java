f71b915b7c97d5210ef08419f6ea4c73b987efa7
CobyBoy <aboutacoby@gmail.com>
2024-01-06 15:26:49 -0300

    Add Mercado Libre bookmark class when /bookmarks gets hit

---
+++ src/main/java/com/meli/wishlist/domain/mercadolibre/Bookmark.java
@@ -0,0 +1,11 @@
+package com.meli.wishlist.domain.mercadolibre;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Bookmark {
+    private String item_id;
+    private String bookmarked_date;
+}
