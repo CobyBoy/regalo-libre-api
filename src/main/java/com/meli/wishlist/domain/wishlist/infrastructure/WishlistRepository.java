fb38984de585f9ba8884616dbd908d6a4bfd4023
CobyBoy <aboutacoby@gmail.com>
2024-01-06 18:21:12 -0300

    Add wishlist repo

---
+++ src/main/java/com/meli/wishlist/domain/wishlist/infrastructure/WishlistRepository.java
@@ -0,0 +1,9 @@
+package com.meli.wishlist.domain.wishlist.infrastructure;
+
+import com.meli.wishlist.domain.wishlist.model.WishList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WishlistRepository extends JpaRepository<WishList, Long> {
+}
