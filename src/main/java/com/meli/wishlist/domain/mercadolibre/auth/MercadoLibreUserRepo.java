6a3f4da92d3ae0c37a46b5a59a27d16558303902
CobyBoy <aboutacoby@gmail.com>
2024-01-06 18:13:05 -0300

    Add repo for meli user

---
+++ src/main/java/com/meli/wishlist/domain/mercadolibre/auth/MercadoLibreUserRepo.java
@@ -0,0 +1,6 @@
+package com.meli.wishlist.domain.mercadolibre.auth;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MercadoLibreUserRepo extends JpaRepository<MercadoLibreUser, String> {
+}
