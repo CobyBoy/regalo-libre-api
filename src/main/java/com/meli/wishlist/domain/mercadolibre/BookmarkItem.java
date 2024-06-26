afe85fe5ae60c79dce10d6f944fbdf2d4bfacc5e
CobyBoy <aboutacoby@gmail.com>
2024-06-26 05:30:09 -0300

    Moved

--- src/main/java/com/meli/wishlist/domain/mercadolibre/BookmarkItem.java
+++
@@ -1,6 +0,0 @@
-package com.meli.wishlist.domain.mercadolibre;
-
-public record BookmarkItem(
-        int code,
-        MercadoLibreProduct body
-) { }
---
+++ src/main/java/com/meli/wishlist/domain/mercadolibre/model/BookmarkItem.java
@@ -0,0 +1,7 @@
+package com.meli.wishlist.domain.mercadolibre.model;
+
+public record BookmarkItem(
+        int code,
+        BookmarkedProduct body
+) {
+}
