56faa6f9fce73cae2685052d450a77d83e3b7c70
CobyBoy <aboutacoby@gmail.com>
2024-06-26 05:14:23 -0300

    Add functions to wishlist repo

--- src/main/java/com/meli/wishlist/domain/wishlist/infrastructure/WishlistRepository.java
+++ src/main/java/com/meli/wishlist/domain/wishlist/infrastructure/WishlistRepository.java
@@ -4,6 +4,11 @@ import com.meli.wishlist.domain.wishlist.model.WishList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WishlistRepository extends JpaRepository<WishList, Long> {
+    List<WishList> findAllByUserId(Long userId);
+
+    WishList findByPublicIdAndIsPrivateFalse(String id);
 }
