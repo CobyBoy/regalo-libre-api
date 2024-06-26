1be9ae52bb3bfe3cbda43ccc3870a82ba9dd8c0a
CobyBoy <aboutacoby@gmail.com>
2024-06-26 05:23:53 -0300

    Remove prop visibility

--- src/main/java/com/meli/wishlist/domain/wishlist/model/dto/EditListDTO.java
+++ src/main/java/com/meli/wishlist/domain/wishlist/model/dto/EditListDTO.java
@@ -1,10 +1,14 @@
 package com.meli.wishlist.domain.wishlist.model.dto;
 
 import com.meli.wishlist.domain.wishlist.model.WishListVisibility;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 
 public record EditListDTO(
         String description,
         String name,
-        WishListVisibility visibility
+        /*@Enumerated(EnumType.STRING)
+        WishListVisibility visibility*/
+        Boolean isPrivate
 ) {
 }
