e48459b0306968124224fdb922d71b78c63cb13b
CobyBoy <aboutacoby@gmail.com>
2024-06-26 05:39:55 -0300

    Moved

--- src/main/java/com/meli/wishlist/domain/mercadolibre/auth/MercadoLibreAccessTokenRepo.java
+++
@@ -1,9 +0,0 @@
-package com.meli.wishlist.domain.mercadolibre.auth;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MercadoLibreAccessTokenRepo extends JpaRepository<MercadoLibreAccessToken, Integer> {
-}
