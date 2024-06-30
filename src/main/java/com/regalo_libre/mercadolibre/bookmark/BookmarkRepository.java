8827cfb67ce80fa97d0f125ebc508c444f9a822a
CobyBoy <aboutacoby@gmail.com>
2024-06-30 15:22:32 -0300

    Add query to find bookmark without user

--- src/main/java/com/regalo_libre/mercadolibre/bookmark/BookmarkRepository.java
+++ src/main/java/com/regalo_libre/mercadolibre/bookmark/BookmarkRepository.java
@@ -5,12 +5,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 @Repository
 public interface BookmarkRepository extends JpaRepository<BookmarkedProduct, Long> {
-    List<BookmarkedProduct> findAllByIdIn(Iterable<String> ids);
+    List<BookmarkedProduct> findAllByIdIn(Collection<String> id);
 
     @Query("SELECT p FROM MercadoLibreUser u JOIN u.bookmarkedProducts p WHERE u.id = :userId ORDER BY p.bookmarkedDate DESC")
     List<BookmarkedProduct> findMercadoLibreProductsByUserId(@Param("userId") Long userId);
+
+    @Query("SELECT B FROM BookmarkedProduct B WHERE B.users is EMPTY")
+    List<BookmarkedProduct> findBookmarkWithoutUser();
 }
