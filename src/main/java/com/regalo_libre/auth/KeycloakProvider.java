7740f6ca74e5bed09dddfa31a2d66ac3dcfa8711
CobyBoy <aboutacoby@gmail.com>
2024-07-08 04:22:04 -0300

    Use config file

--- src/main/java/com/regalo_libre/auth/KeycloakProvider.java
+++ src/main/java/com/regalo_libre/auth/KeycloakProvider.java
@@ -1,39 +1,34 @@
 package com.regalo_libre.auth;
 
+import lombok.RequiredArgsConstructor;
 import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.admin.client.resource.RealmResource;
 import org.keycloak.admin.client.resource.UsersResource;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@Service
 public class KeycloakProvider {
-    private static final String SERVER_URL = "http://localhost:9090";
-    private static final String REALM_NAME = "wishlist-realm-dev";
-    private static final String REALM_MASTER = "master";
-    private static final String ADMIN_CLI = "admin-cli";
-    private static final String USER_CONSOLE = "cobyadmin";
-    private static final String PASSWORD_CONSOLE = "35396973";
-    private static final String CLIENT_SECRET = "iEywx0WMbiJ8I7hMQquRMUa3iVL3vFmA";
+    private final KeycloakConfig keycloakConfig;
 
-    private KeycloakProvider() {
-    }
-
-    public static RealmResource getRealmResource() {
+    public RealmResource getRealmResource() {
         Keycloak keycloak = KeycloakBuilder.builder()
-                .serverUrl(SERVER_URL)
-                .realm(REALM_MASTER)
-                .clientId(ADMIN_CLI)
-                .username(USER_CONSOLE)
-                .password(PASSWORD_CONSOLE)
-                .clientSecret(CLIENT_SECRET)
+                .serverUrl(keycloakConfig.getServerUrl())
+                .realm(keycloakConfig.getRealmMaster())
+                .clientId(keycloakConfig.getAdminCli())
+                .username(keycloakConfig.getUserConsole())
+                .password(keycloakConfig.getPasswordConsole())
+                .clientSecret(keycloakConfig.getClientSecret())
                 .resteasyClient(new ResteasyClientBuilderImpl()
                         .connectionPoolSize(10)
                         .build())
                 .build();
-        return keycloak.realm(REALM_NAME);
+        return keycloak.realm(keycloakConfig.getRealmName());
     }
 
-    public static UsersResource getUserResource() {
+    public UsersResource getUserResource() {
         RealmResource realmResource = getRealmResource();
         return realmResource.users();
     }
