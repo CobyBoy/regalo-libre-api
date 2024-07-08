3c5f49357b65cf26fe82c05b98152a62c1512ded
CobyBoy <aboutacoby@gmail.com>
2024-07-08 04:15:26 -0300

    Add keycloak config file

---
+++ src/main/java/com/regalo_libre/auth/KeycloakConfig.java
@@ -0,0 +1,24 @@
+package com.regalo_libre.auth;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class KeycloakConfig {
+    @Value("${keycloak.admin_cli}")
+    private String adminCli;
+    @Value("${keycloak.realm_name}")
+    private String realmName;
+    @Value("${keycloak.client_secret}")
+    private String clientSecret;
+    @Value("${keycloak.server_url}")
+    private String serverUrl;
+    @Value("${keycloak.realm_master}")
+    private String realmMaster;
+    @Value("${keycloak.user_console}")
+    private String userConsole;
+    @Value("${keycloak.password_console}")
+    private String passwordConsole;
+}
