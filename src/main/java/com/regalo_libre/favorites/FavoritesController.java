cc8395090e0330be2bbca2364c1e8d737700a91c
CobyBoy <aboutacoby@gmail.com>
2024-07-09 19:20:04 -0300

    Change dto on favorites controller

--- src/main/java/com/regalo_libre/favorites/FavoritesController.java
+++ src/main/java/com/regalo_libre/favorites/FavoritesController.java
@@ -1,7 +1,5 @@
 package com.regalo_libre.favorites;
 
-import com.regalo_libre.auth.IKeycloakService;
-import com.regalo_libre.mercadolibre.bookmark.BookmarkedProduct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,13 +8,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/favorites")
-@CrossOrigin(origins = "https://localhost:4200")
 @RequiredArgsConstructor
 public class FavoritesController {
     private final IFavoritesService favoritesService;
 
     @GetMapping
-    public ResponseEntity<List<BookmarkedProduct>> getFavorites(@RequestParam Long userId) {
+    public ResponseEntity<List<FavoritesDTO>> getFavorites(@RequestParam Long userId) {
         return ResponseEntity.ok(favoritesService.getAllFavorites(userId));
     }
 }
