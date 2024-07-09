2ea2a26dceb4bff876ff826584100ff705dab94f
CobyBoy <aboutacoby@gmail.com>
2024-07-09 19:28:15 -0300

    Update with dto

--- src/main/java/com/regalo_libre/mercadolibre/auth/MercadoLibreAuthController.java
+++ src/main/java/com/regalo_libre/mercadolibre/auth/MercadoLibreAuthController.java
@@ -1,6 +1,6 @@
 package com.regalo_libre.mercadolibre.auth;
 
-import com.regalo_libre.mercadolibre.auth.model.MercadoLibreUser;
+import com.regalo_libre.mercadolibre.auth.model.MercadoLibreUserDTO;
 import com.regalo_libre.mercadolibre.auth.service.MercadoLibreAuthClientServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +12,8 @@ import org.springframework.web.bind.annotation.*;
 public class MercadoLibreAuthController {
     private final MercadoLibreAuthClientServiceImpl mercadoLibreAuthClientServiceImpl;
 
-    @CrossOrigin(origins = "https://localhost:4200")
     @GetMapping
-    public ResponseEntity<MercadoLibreUser> getMercadoLibreUserData(@RequestParam(name = "authorizationCode") String authorizationCode) {
+    public ResponseEntity<MercadoLibreUserDTO> getMercadoLibreUserData(@RequestParam(name = "authorizationCode") String authorizationCode) {
         return ResponseEntity.ok(mercadoLibreAuthClientServiceImpl.getMercadoLibreUserData(authorizationCode));
     }
 }
