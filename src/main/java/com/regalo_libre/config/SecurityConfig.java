6244efbdbe445d945658dbc30b93c4043fa101af
CobyBoy <aboutacoby@gmail.com>
2024-07-09 19:19:29 -0300

    Add CORS Config

--- src/main/java/com/regalo_libre/config/SecurityConfig.java
+++ src/main/java/com/regalo_libre/config/SecurityConfig.java
@@ -8,6 +8,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -20,14 +26,27 @@ public class SecurityConfig {
                 .authorizeHttpRequests(http -> {
                     http.requestMatchers(HttpMethod.POST, "/api/user/**").permitAll();
                     http.requestMatchers(HttpMethod.GET, "/api/token/**").permitAll();
+                    http.requestMatchers(HttpMethod.GET, "/api/lists/user/**").permitAll();
                     http.requestMatchers(HttpMethod.GET, "/api/lists/public/**").permitAll();
-                    http.requestMatchers(HttpMethod.GET, "/api/profile/public/**").permitAll();
+                    http.requestMatchers(new RegexRequestMatcher("/api/profile/[^/]+", "GET")).permitAll();
                     http.anyRequest().authenticated();
                 })
+                .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
                 .oauth2ResourceServer(oauth2ResourceServerCustomizer ->
                         oauth2ResourceServerCustomizer.jwt(jwtCustomizer -> {
                         }))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("https://localhost:4200"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
