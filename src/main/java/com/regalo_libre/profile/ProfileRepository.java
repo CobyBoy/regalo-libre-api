358ec8712f635511a5335a2cb24b2346d023863f
CobyBoy <aboutacoby@gmail.com>
2024-07-09 19:26:12 -0300

    Add query to find profile by nickname

--- src/main/java/com/regalo_libre/profile/ProfileRepository.java
+++ src/main/java/com/regalo_libre/profile/ProfileRepository.java
@@ -1,8 +1,14 @@
 package com.regalo_libre.profile;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
+    @Query("SELECT p FROM Profile p WHERE p.appNickname  = :username AND p.isPrivate = false")
+    Optional<Profile> findByAppNickname(@Param("username") String username);
 }
