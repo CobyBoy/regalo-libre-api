6f53323851f5b16c71c646b561af695e3c2ccf4b
CobyBoy <aboutacoby@gmail.com>
2024-07-19 19:23:58 -0300

    RL-7 | Add try catch to profile controller

--- src/main/java/com/regalo_libre/auth/OAuthProfileController.java
+++ src/main/java/com/regalo_libre/auth/OAuthProfileController.java
@@ -1,6 +1,7 @@
 package com.regalo_libre.auth;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -8,28 +9,35 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class OAuthProfileController {
     private final OauthPropertiesConfig oauthPropertiesConfig;
 
     @GetMapping("/api/v1/oauth-profile")
-    public ResponseEntity getProfile(@RequestHeader("Authorization") String authorizationHeader) {
+    public ResponseEntity<OAuthUserInfo> getProfile(@RequestHeader("Authorization") String authorizationHeader) {
         RestTemplate restTemplate = new RestTemplate();
-
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", authorizationHeader);
-
         HttpEntity<?> entity = new HttpEntity<>(headers);
 
-        OAuthUserInfo response = restTemplate.exchange(
-                oauthPropertiesConfig.getUserInfoUrl(),
-                HttpMethod.GET,
-                entity,
-                OAuthUserInfo.class
-        ).getBody();
+        OAuthUserInfo response;
+        try {
+            response = restTemplate.exchange(
+                    oauthPropertiesConfig.getUserInfoUrl(),
+                    HttpMethod.GET,
+                    entity,
+                    OAuthUserInfo.class
+            ).getBody();
+        } catch (HttpClientErrorException e) {
+            throw e;
+        }
+
+        log.info("Getting user profile {}", response);
 
         return ResponseEntity.ok(response);
     }
