416e88f0e665b8c86120ca787a2af18617f51028
CobyBoy <aboutacoby@gmail.com>
2024-07-20 02:52:51 -0300

    RL-11 | Move profile logic to service from controller

--- src/main/java/com/regalo_libre/auth/OAuthUserServiceImpl.java
+++ src/main/java/com/regalo_libre/auth/OAuthUserServiceImpl.java
@@ -3,12 +3,41 @@ package com.regalo_libre.auth;
 import com.regalo_libre.auth.model.OAuthUser;
 import com.regalo_libre.auth.repository.OAuthUserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OAuthUserServiceImpl {
     private final OAuthUserRepository repository;
+    private final OauthPropertiesConfig oauthPropertiesConfig;
+
+    public OAuthUserInfo getOauthUserInfo(String authorizationHeader) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", authorizationHeader);
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        OAuthUserInfo response;
+        try {
+            response = restTemplate.exchange(
+                    oauthPropertiesConfig.getUserInfoUrl(),
+                    HttpMethod.GET,
+                    entity,
+                    OAuthUserInfo.class
+            ).getBody();
+        } catch (HttpClientErrorException e) {
+            throw e;
+        }
+        log.info("Getting user profile {}", response);
+        return response;
+    }
 
     public OAuthUser createOauthUser(OAuthUserInfo response) {
         return repository.save(OAuthUser.builder()
