e57af74822170f3693dff47025d46efc5c59e27a
CobyBoy <aboutacoby@gmail.com>
2024-07-20 04:37:40 -0300

    RL-12 | Update WishList entity to use auth user instead of meli user

--- src/main/java/com/regalo_libre/auth/repository/OAuthUserRepository.java
+++ src/main/java/com/regalo_libre/auth/repository/OAuthUserRepository.java
@@ -3,5 +3,5 @@ package com.regalo_libre.auth.repository;
 import com.regalo_libre.auth.model.OAuthUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OAuthUserRepository extends JpaRepository<OAuthUser, String> {
-}
+    public interface OAuthUserRepository extends JpaRepository<OAuthUser, Long> {
+    }
