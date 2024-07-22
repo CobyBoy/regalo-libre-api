6557ca6f4cf446149f2e1e4cf6a0ac7c9beeaa25
CobyBoy <aboutacoby@gmail.com>
2024-07-22 01:23:52 -0300

    RL-18 | Remove actions from listener to management api service

--- src/main/java/com/regalo_libre/auth/OAuthManagementApiListener.java
+++ src/main/java/com/regalo_libre/auth/OAuthManagementApiListener.java
@@ -1,7 +1,6 @@
 package com.regalo_libre.auth;
 
 import com.regalo_libre.auth.model.OAuthUser;
-import com.regalo_libre.mercadolibre.auth.MercadoLibreAccessTokenService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
@@ -12,19 +11,13 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class OAuthManagementApiListener implements ApplicationListener<OAuthUserCreatedEvent> {
     private final OAuthManagementApiService managementApiService;
-    private final OAuthManagementApiRepository oAuthManagementApiRepository;
-    private final MercadoLibreAccessTokenService mercadoLibreAccessTokenService;
 
     @Override
     public void onApplicationEvent(OAuthUserCreatedEvent event) {
         log.info("Handling event {}", event.getoAuthUser());
         OAuthUser user = event.getoAuthUser();
-        OAuthManagementApiToken token = managementApiService.getManagementApiToken();
-        token.setUserId(user.getId());
-        oAuthManagementApiRepository.save(token);
-        var mercadoLibreAccessToken = managementApiService.getMercadoLibreUserTokenInfoFromApiManagement(token.getAccessToken(), user.getSub());
-        mercadoLibreAccessToken.setUserId(user.getId());
-        mercadoLibreAccessTokenService.saveAccessToken(mercadoLibreAccessToken);
+        OAuthManagementApiToken token = managementApiService.getManagementApiToken(user.getId());
+        managementApiService.getMercadoLibreUserTokenInfoFromApiManagement(token.getAccessToken(), user.getSub(), user.getId());
     }
 
 }
--- src/main/java/com/regalo_libre/auth/OAuthManagementApiService.java
+++ src/main/java/com/regalo_libre/auth/OAuthManagementApiService.java
@@ -2,6 +2,7 @@ package com.regalo_libre.auth;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.regalo_libre.auth.config.OauthPropertiesConfig;
+import com.regalo_libre.mercadolibre.auth.MercadoLibreAccessTokenService;
 import com.regalo_libre.mercadolibre.auth.model.MercadoLibreAccessToken;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.*;
@@ -14,8 +15,10 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class OAuthManagementApiService {
     private final OauthPropertiesConfig oauthPropertiesConfig;
+    private final OAuthManagementApiRepository oAuthManagementApiRepository;
+    private final MercadoLibreAccessTokenService mercadoLibreAccessTokenService;
 
-    public OAuthManagementApiToken getManagementApiToken() {
+    public OAuthManagementApiToken getManagementApiToken(Long userId) {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -28,15 +31,18 @@ public class OAuthManagementApiService {
 
         HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(map, headers);
 
-        return restTemplate.exchange(
+        var response = restTemplate.exchange(
                 oauthPropertiesConfig.getApiManagementTokenUrl(),
                 HttpMethod.POST,
                 requestEntity,
                 OAuthManagementApiToken.class
         ).getBody();
+        assert response != null;
+        response.setUserId(userId);
+        return oAuthManagementApiRepository.save(response);
     }
 
-    public MercadoLibreAccessToken getMercadoLibreUserTokenInfoFromApiManagement(String accessToken, String sub) {
+    public void getMercadoLibreUserTokenInfoFromApiManagement(String accessToken, String sub, Long userId) {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(accessToken);
@@ -51,7 +57,8 @@ public class OAuthManagementApiService {
                 MercadoLibreTokenInfo.class
         ).getBody();
         assert response != null;
-        return response.mercadoLibreAccessToken;
+        response.mercadoLibreAccessToken.setUserId(userId);
+        mercadoLibreAccessTokenService.saveAccessToken(response.mercadoLibreAccessToken);
     }
 
     private static class MercadoLibreTokenInfo {
