b7b1f083497956127f91d90f167cdbd8cf67aee1
CobyBoy <aboutacoby@gmail.com>
2024-08-04 22:28:52 -0300

    RL-22 | Add logout endpoint

--- src/main/java/com/regalo_libre/login/LoginController.java
+++ src/main/java/com/regalo_libre/login/LoginController.java
@@ -4,7 +4,9 @@ import com.regalo_libre.auth.config.OauthPropertiesConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +23,18 @@ public class LoginController {
                 "&scope=openid profile";
 
         return new RedirectView(authorizationUrl);
+    }
+
+    @GetMapping("/api/v1/logout")
+    public RedirectView logout() {
+        RestTemplate restTemplate = new RestTemplate();
+        String logoutRedirect = "https://localhost:4200/logout";
+        String url = UriComponentsBuilder.fromHttpUrl(oauthPropertiesConfig.getLogoutUrl())
+                .queryParam("client_id", oauthPropertiesConfig.getClientId())
+                .queryParam("returnTo", logoutRedirect)
+                .toUriString();
+
+        restTemplate.getForObject(url, String.class);
+        return new RedirectView(logoutRedirect);
     }
 }
