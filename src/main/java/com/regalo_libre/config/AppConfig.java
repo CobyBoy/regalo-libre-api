b48f124bb105b591dbbf0ff78ed83108c295ce56
CobyBoy <aboutacoby@gmail.com>
2024-08-25 16:34:28 -0300

    RL-68 | Add async for events and change cache name

--- src/main/java/com/regalo_libre/config/AppConfig.java
+++ src/main/java/com/regalo_libre/config/AppConfig.java
@@ -8,17 +8,20 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
+@EnableAsync
 public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
@@ -38,11 +41,22 @@ public class AppConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager("userBookmarks");
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("userBookmarksCache");
         cacheManager.setCaffeine(Caffeine.newBuilder()
-                .expireAfterWrite(60, TimeUnit.MINUTES)
+                .expireAfterWrite(1, TimeUnit.MINUTES)
                 .maximumSize(100));
         return cacheManager;
+    }
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("async-task-");
+        executor.initialize();
+        return executor;
     }
 
 }
