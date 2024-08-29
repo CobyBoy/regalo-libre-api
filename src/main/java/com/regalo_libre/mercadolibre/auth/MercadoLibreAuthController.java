7c1afddee590ee05f0f47eb324a8987974c9e2f2
CobyBoy <aboutacoby@gmail.com>
2024-08-28 21:43:47 -0300

    RL-75 | Add logs in meli login

--- src/main/java/com/regalo_libre/mercadolibre/auth/MercadoLibreAuthController.java
+++ src/main/java/com/regalo_libre/mercadolibre/auth/MercadoLibreAuthController.java
@@ -1,11 +1,13 @@
 package com.regalo_libre.mercadolibre.auth;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("api/v1/meli")
 public class MercadoLibreAuthController {
@@ -16,6 +18,7 @@ public class MercadoLibreAuthController {
     @GetMapping("code")
     public RedirectView getMercadoLibreUserData(@RequestParam(name = "code") String code) {
         mercadoLibreAccessTokenService.exchangeCodeForToken(code);
+        log.info("Redirecting after log in with Mercado Libre to {}", uiUrl + "/user/lists");
         return new RedirectView(uiUrl + "/user/lists");
     }
 }
