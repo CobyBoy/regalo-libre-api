463588ba4f3ad4d6d36abd251d8250936f645960
CobyBoy <aboutacoby@gmail.com>
2024-10-18 22:45:38 -0300

    RL-111 | Add followers and followee info when getting the profile info

--- src/main/java/com/regalo_libre/profile/ProfileService.java
+++ src/main/java/com/regalo_libre/profile/ProfileService.java
@@ -3,7 +3,11 @@ package com.regalo_libre.profile;
 public interface ProfileService {
     Profile getProfile(Long userId);
 
-    PublicProfileDTO findPublicProfileByUserNickname(String username);
+    PublicProfileDTO findPublicProfileByUserNickname(String username, Long followerId);
+
+    /*PublicProfileDTO findPublicProfileByProfileId(Long profileId, Long followerId);*/
 
     EditProfileDTO editProfile(EditProfileDTO profile);
+
+    Profile findProfileByAppNicknameAndIsPrivateFalse(String username);
 }
