d298a86a4a0d320abce87472d102d8e8b690cd97
CobyBoy <aboutacoby@gmail.com>
2024-10-18 22:43:23 -0300

    RL-111 | Add queries on repositories

--- src/main/java/com/regalo_libre/auth/repository/Auth0UserRepository.java
+++ src/main/java/com/regalo_libre/auth/repository/Auth0UserRepository.java
@@ -3,5 +3,8 @@ package com.regalo_libre.auth.repository;
 import com.regalo_libre.auth.model.Auth0User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface Auth0UserRepository extends JpaRepository<Auth0User, Long> {
+    Optional<Auth0User> findByProfile_ProfileId(Long profileId);
 }
